8620f8f5ac97e2e4b67dfaef0d7c00c7842dee74
Ziad <ze068@live.mdx.ac.uk>
2023-11-27 12:00:21 +0400

    Fixed Errors

--- broken_travel.py
+++ broken_travel.py
@@ -9,8 +9,8 @@
 year = int(input("Greetings! What is your year of origin? "))
 
 if year <= 1900:
-    print ('Woah, that's the past!')
-elif year > 1900 && year < 2020:
+    print ("Woah, that's the past!")
+elif  1900 < year <= 2020:
     print ("That's totally the present!")
-elif:
+else:
     print ("Far out, that's the future!!")
